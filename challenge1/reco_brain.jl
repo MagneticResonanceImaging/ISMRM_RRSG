9e463ec886a3949266b2dd581de6c5e740d8e92d
migrosser <mi.grosser@uke.de>
2020-06-04 10:29:38 +0200

    generate plots for brain data

--- challenge1/reco_brain.jl
+++ challenge1/reco_brain.jl
@@ -1,53 +1,93 @@
-using PyPlot, HDF5, MRIReco
+using PyPlot, HDF5, MRIReco, LinearAlgebra
 
 filename = "rawdata_brain_radial_96proj_12ch.h5"
 data = permutedims(h5read(filename, "rawdata"),[3,2,1,4])
 traj = permutedims(h5read(filename, "trajectory"),[3,2,1])
 N = 300
 
-# Convert the loaded data into an Acquisition data object
-# Trajectory needs to be scaled bewtween [-0.5,0.5]. The remaining
-# operations are just reshaping / changing dimension order, etc.
+#############################################
+# load data and form Acquisition data object
+##############################################
 tr = Trajectory(reshape(traj[1:2,:,:],2,:) ./300, 96, 512, circular=false)
 dat = Array{Array{Complex{Float64},2},3}(undef,1,1,1)
 dat[1,1,1] = 1.e8.*reshape(data,:,12)
 acqData = AcquisitionData(tr, dat, encodingSize=[N,N,1])
 
-# single channel reconstruction of fully sampled data
-params = Dict{Symbol, Any}()
-params[:reco] = "standard"
-params[:reconSize] = (N,N)
-params[:regularization] = "L2"
-params[:solver] = "cgnr"
-params[:λ] = 0.0
-params[:iterations] = 1
-Ireco = reconstruction(acqData, params)
-
-figure("single-channel reco")
-imshow(abs.(Ireco[:,:,1,1,1]),cmap="gray")
-
+################################
 # generate coil sensitivity maps
+################################
+@info "Espirit"
 acqDataCart = regrid2d(acqData, (N,N); cgnr_iter=3)
 sensitivity = espirit(acqDataCart,(6,6),30,eigThresh_1=0.02, eigThresh_2=0.98)
 
-# generate undersampled data
-redFac = 4.0
-# we directly generate an undersampled trajectory
-# this saves unnecessary computations during reconstruction
-acqDataSub = convertUndersampledData(sample_kspace(acqData, redFac, "regular"))
-
-# CG-SENSE reconstruction
+##########################
+# reference reconstruction
+##########################
+@info "reference reco"
 params = Dict{Symbol, Any}()
 params[:reco] = "multiCoil"
 params[:reconSize] = (N,N)
 params[:regularization] = "L2"
-params[:λ] = 0.0
-params[:iterations] = 10
+params[:λ] = 1.e-2
+params[:iterations] = 100
 params[:solver] = "cgnr"
 params[:senseMaps] = reshape(sensitivity, N, N, 1, 12)
 
-IrecoSENSE = reconstruction(acqDataSub, params)
+img_ref = reconstruction(acqData, params).data
+
+##############################
+# undersampled reconstructions
+##############################
+@info "undersampled reco"
+rf = [1,2,3,4]
+Δ = Vector{Vector{Float64}}(undef,4)
+img_cg = Vector{Array{ComplexF64,5}}(undef,4)
+img_cg1 = Vector{Array{ComplexF64,5}}(undef,4)
+img_sc = Vector{Array{ComplexF64,5}}(undef,4)
+params[:iterations] = 20
+for r in rf
+  @info "r=$r"
+  # undersample profiles
+  acqDataSub = convertUndersampledData(sample_kspace(acqData, Float64.(r), "regular"))
+  # SENSE reconstruction while monitoring error
+  params[:reco] = "multiCoil"
+  params[:solverInfo] = SolverInfo(store_solutions=true)
+  img_cg[r] = reconstruction(acqDataSub, params).data
+  img_cg1[r] = reshape(params[:solverInfo].x_iter[2],N,N,1,1,1)
+  Δ[r] = [norm(vec(img_ref).-params[:solverInfo].x_iter[i])/norm(vec(img_ref)) for i=1:params[:iterations]]
+  # single-channel reconstruction
+  params[:reco] = "direct"
+  img_sc[r] = reconstruction(acqDataSub, params).data
+end
 
 
-figure(2)
-imshow(abs.(IrecoSENSE[:,:,1,1,1]),cmap="gray")
+##############
+# plot figures
+##############
+figure("brain reconstruction error")
+plot(log.(Δ[1]),label="r=1")
+plot(log.(Δ[2]),label="r=2")
+plot(log.(Δ[3]),label="r=3")
+plot(log.(Δ[4]),label="r=4")
+xlabel("iteration numbers")
+ylabel("log(Δ)")
+legend()
+
+figure("brain reconstructions")
+subplot(4,3,1); title("Single coil")
+subplot(4,3,2); title("Initial")
+subplot(4,3,3); title("Final")
+for r in rf
+  # plots
+  subplot(4,3,(r-1)*3+1)
+  imshow(abs.(img_sc[r][:,:,1,1,1]),cmap="gray")
+  ylabel("r=$r")
+  xticks([]);yticks([])
+  subplot(4,3,(r-1)*3+2)
+  imshow(abs.(img_cg1[r][:,:,1,1,1]),cmap="gray")
+  xticks([]);yticks([])
+  subplot(4,3,(r-1)*3+3)
+  imshow(abs.(img_cg[r][:,:,1,1,1]),cmap="gray")
+  xticks([]);yticks([])
+end
+tight_layout()
